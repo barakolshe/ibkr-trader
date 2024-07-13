5119b2143ac3089e0f97ba6331904b25673494de
barak-kovrr <barako@kovrr.com>
2024-07-13 18:20:28 +0300

    done

--- src/main.py
+++ src/main.py
@@ -1,13 +1,41 @@
 from datetime import datetime
+import time
 from consts.time_consts import TIMEZONE
+from consts.trading_consts import get_start_datetime
 from controllers.trading.fetcher import (
     get_actions,
 )
 from controllers.trading.strategy import PaperStrategy
 from controllers.trading.trader import BaseTrader, compare_dates
 import arrow
+import boto3
 
 US_EXCHANGES = ["NYSE", "NASDAQ", "AMEX", "NYSEA"]
+AMAZON_BUCKET_NAME: str = "barak-trading-bucket"
+
+
+def sleep_until(target_datetime: datetime) -> None:
+    diff = target_datetime - arrow.now(tz="US/Eastern").datetime
+    time.sleep(diff.total_seconds())
+
+
+def check_kill_all_command() -> bool:
+    s3_client = boto3.client("s3")
+    try:
+        s3_client.get_object(Bucket=AMAZON_BUCKET_NAME, Key="exit2.json")
+        s3_client.delete_objects(
+            Bucket=AMAZON_BUCKET_NAME,
+            Delete={
+                "Objects": [
+                    {
+                        "Key": "exit2.json",
+                    },
+                ],
+            },
+        )
+        return True
+    except:
+        return False
 
 
 def test_strategy() -> None:
@@ -33,4 +61,18 @@ def live_trade() -> None:
 
 
 if __name__ == "__main__":
-    test_strategy()
+    while True:
+        days_shift = (
+            7 - arrow.now(tz="US/Eastern").weekday()
+            if arrow.now(tz="US/Eastern").weekday() > 5
+            else 1
+        )
+        sleep_until(
+            arrow.now(tz="US/Eastern")
+            .shift(day=days_shift)
+            .replace(hour=10, minute=45, second=0)
+            .datetime
+        )
+        if check_kill_all_command():
+            break
+        live_trade()
