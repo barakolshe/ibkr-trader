e3c5e421fe90afe5ff909cb6c4e8630040b712fd
barak-kovrr <barako@kovrr.com>
2024-07-18 18:08:37 +0300

    dsad

--- src/controllers/trading/fetcher.py
+++ src/controllers/trading/fetcher.py
@@ -18,7 +18,7 @@ def get_actions(exchanges: list[str], all: bool) -> list[Evaluation]:
     password = os.environ.get("MONGODB_PASSWORD")
     cluster_name = os.environ.get("MONGODB_CLUSTER_NAME")
     client: MongoClient[Any] = MongoClient[Any](
-        f"mongodb://{username}:{password}@{cluster_name}/?retryWrites=true&w=majority&appName=best-friend",
+        f"mongodb+srv://{username}:{password}@{cluster_name}/?retryWrites=true&w=majority&appName=best-friend"
     )
     db = client["trading"]
     collection = db["actions"]
